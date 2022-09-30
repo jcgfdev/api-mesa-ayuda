b7287f0e53d6b0a40ecb5b13139e879ee6b3b7d2
jcgfdev <jcgf1406@gmail.com>
2022-09-29 20:00:51 -0500

    subida inicial del proyecto de mesa de ayuda al repositorio

---
+++ src/main/java/com/enyoi/apimesaayuda/ApiMesaAyudaApplication.java
@@ -0,0 +1,13 @@
+package com.enyoi.apimesaayuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMesaAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMesaAyudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enyoi/apimesaayuda/ApiMesaAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enyoi.apimesaayuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMesaAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

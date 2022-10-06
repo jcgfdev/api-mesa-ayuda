fb5307d6bb8ea95e29298c93e89757f54935b73c
Jnnio <jnnisuarez@gmail.com>
2022-10-05 21:01:12 -0500

    Se crea entidades para solicitudes y seguimientos.

---
+++ src/main/java/com/enyoi/apimesaayuda/aplicacion/entities/Seguimientos.java
@@ -0,0 +1,33 @@
+package com.enyoi.apimesaayuda.aplicacion.entities;
+
+import com.enyoi.apimesaayuda.security.entities.Usuarios;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Seguimientos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @JoinColumn(name = "solicitudes_id",referencedColumnName ="id",nullable = false)
+    @ManyToOne(optional = false)
+    private Solicitudes solicitudesId;
+    @NotBlank
+    @Column(name = "titulo",nullable = false,length = 50)
+    private String titulo;
+    @Column(name = "fecha_realizado",nullable = false)
+    private Date fechaRealizado;
+    @NotBlank
+    @Column(name = "descripcion",nullable = false,length = 2000)
+    private String descripcion;
+    @JoinColumn(name = "responsable_id",referencedColumnName ="id",nullable = false)
+    @ManyToOne(optional = false)
+    private Usuarios responsableId;
+}

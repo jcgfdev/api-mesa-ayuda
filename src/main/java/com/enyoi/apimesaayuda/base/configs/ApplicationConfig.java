310b6ae8ee11facc8f7f401087e04e482b06a876
jcgfdev <jcgf1406@gmail.com>
2022-10-26 20:32:41 -0500

    adicion swagger aplicacion

--- src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
+++ src/main/java/com/enyoi/apimesaayuda/base/configs/ApplicationConfig.java
@@ -1,5 +1,8 @@
 package com.enyoi.apimesaayuda.base.configs;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +12,14 @@ public class ApplicationConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
     }
 
 }

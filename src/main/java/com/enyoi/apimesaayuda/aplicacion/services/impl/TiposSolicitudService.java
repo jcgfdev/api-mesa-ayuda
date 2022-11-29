b3b87e021dfa51cbd8b4e4c341bea667ef373706
Jnnio <jnnisuarez@gmail.com>
2022-11-28 19:17:40 -0500

    Creación LogsTiposSolicitud

--- src/main/java/com/enyoi/apimesaayuda/aplicacion/services/impl/TiposSolicitudService.java
+++ src/main/java/com/enyoi/apimesaayuda/aplicacion/services/impl/TiposSolicitudService.java
@@ -2,9 +2,13 @@ package com.enyoi.apimesaayuda.aplicacion.services.impl;
 
 import com.enyoi.apimesaayuda.aplicacion.dtos.TiposSolicitudDTO;
 import com.enyoi.apimesaayuda.aplicacion.entities.TiposSolicitud;
+import com.enyoi.apimesaayuda.aplicacion.entities.logs.LogsTiposSolicitud;
+import com.enyoi.apimesaayuda.aplicacion.payloads.requests.CrearTiposSolicitudRequest;
 import com.enyoi.apimesaayuda.aplicacion.payloads.requests.TiposSolicitudRequest;
 import com.enyoi.apimesaayuda.aplicacion.repositories.TiposSolicitudRepository;
+import com.enyoi.apimesaayuda.aplicacion.repositories.logs.LogsTiposSolicitudRepository;
 import com.enyoi.apimesaayuda.aplicacion.services.ITiposSolicitudService;
+import com.enyoi.apimesaayuda.base.enums.Acciones;
 import com.enyoi.apimesaayuda.base.exceptions.AlreadyExists;
 import com.enyoi.apimesaayuda.base.exceptions.NotDataFound;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +25,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TiposSolicitudService implements ITiposSolicitudService {
     private final ModelMapper modelMapper;
+    private final LogsTiposSolicitudRepository logsTiposSolicitudRepository;
     private static final String NOEXISTENDATOS = "No esxisten datos";
     private final TiposSolicitudRepository tiposSolicitudRepository;
 
@@ -63,13 +69,20 @@ public class TiposSolicitudService implements ITiposSolicitudService {
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public TiposSolicitudDTO create(String tipoSolicitud) {
-        Optional<TiposSolicitud> tiposSolicitudOptional = tiposSolicitudRepository.findByTipoSolicitud(tipoSolicitud);
+    public TiposSolicitudDTO create(CrearTiposSolicitudRequest crearTiposSolicitudRequest) {
+        Optional<TiposSolicitud> tiposSolicitudOptional = tiposSolicitudRepository
+                .findByTipoSolicitud(crearTiposSolicitudRequest.getTipoSolicitud());
         if (tiposSolicitudOptional.isPresent()) {
             throw new AlreadyExists("Tipo solicitud ya existe");
         } else {
             TiposSolicitud tiposSolicitud = new TiposSolicitud();
-            tiposSolicitud.setTipoSolicitud(tipoSolicitud);
+            tiposSolicitud.setTipoSolicitud(crearTiposSolicitudRequest.getTipoSolicitud());
+            LogsTiposSolicitud logsTiposSolicitud = LogsTiposSolicitud.builder()
+                    .usuario(crearTiposSolicitudRequest.getUsuario())
+                    .acciones(Acciones.CREATED)
+                    .tiposSolicitud(tiposSolicitud.getTipoSolicitud())
+                    .fechalog(new Date()).build();
+            logsTiposSolicitudRepository.save(logsTiposSolicitud);
             return modelMapper.map(tiposSolicitudRepository.save(tiposSolicitud), TiposSolicitudDTO.class);
         }
 
@@ -83,6 +96,12 @@ public class TiposSolicitudService implements ITiposSolicitudService {
             tipoSolicitudGuardar.setTipoSolicitud(tiposSolicitudRequests.getTipoSolicitud() != null
                     ? tiposSolicitudRequests.getTipoSolicitud()
                     : tipoSolicitudGuardar.getTipoSolicitud());
+            LogsTiposSolicitud logsTiposSolicitud = LogsTiposSolicitud.builder()
+                    .usuario(tiposSolicitudRequests.getUsuario())
+                    .acciones(Acciones.UPDATE)
+                    .tiposSolicitud(tiposSolicitudRequests.getTipoSolicitud())
+                    .fechalog(new Date()).build();
+            logsTiposSolicitudRepository.save(logsTiposSolicitud);
             return modelMapper.map(tiposSolicitudRepository.save(tipoSolicitudGuardar), TiposSolicitudDTO.class);
         } else {
             throw new NotDataFound(NOEXISTENDATOS);
@@ -91,13 +110,19 @@ public class TiposSolicitudService implements ITiposSolicitudService {
 
 
     @Override
-    public String delete(Long id) {
+    public String delete(Long id, String usuarios) {
         Optional<TiposSolicitud> tiposSolicitudOptional = Optional.ofNullable(tiposSolicitudRepository.findById(id)
                 .orElseThrow(() -> new NotDataFound("No existe el Tipo solicitud: " + id)));
-        if (tiposSolicitudOptional.isPresent()){
+        if (tiposSolicitudOptional.isPresent()) {
             tiposSolicitudRepository.deleteById(id);
+            LogsTiposSolicitud logsTiposSolicitud = LogsTiposSolicitud.builder()
+                    .usuario(usuarios)
+                    .acciones(Acciones.DELETE)
+                    .tiposSolicitud(tiposSolicitudOptional.get().getTipoSolicitud())
+                    .fechalog(new Date()).build();
+            logsTiposSolicitudRepository.save(logsTiposSolicitud);
             return modelMapper.map(tiposSolicitudOptional.get(), TiposSolicitudDTO.class).getTipoSolicitud() + "Tipo solicitud eliminado con Exito";
-        }else {
+        } else {
             throw new NotDataFound(NOEXISTENDATOS);
         }
 
